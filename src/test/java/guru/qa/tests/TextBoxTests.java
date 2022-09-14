65c03ca15cbf6dec4d2f58d8f2074474b6df81b5
Алексей <krysov-95@mail.ru>
2022-09-14 19:02:47 +0600

    test demoqa 14.09

---
+++ src/test/java/guru/qa/tests/TextBoxTests.java
@@ -0,0 +1,18 @@
+package guru.qa.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("alex@qaguru.com");
+        $("#currentAddress").setValue("some street 1");
+        $("#permanentAddress").setValue("another street 2");
+        $("#submit").click();
+    }
+}
