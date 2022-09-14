78e536bb56c68b6f4afe021eb481988862207625
Алексей <krysov-95@mail.ru>
2022-09-14 15:52:19 +0600

    added tests

---
+++ src/test/java/guru/qa/DemoQATest.java
@@ -0,0 +1,37 @@
+package guru.qa;
+
+import org.junit.jupiter.api.*;
+
+public class DemoQATest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all tests!\n");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all tests!\n");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before test!\n");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After test!\n");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("I love you QA.GURU\n");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("I love you QA.GURU, second test\n");
+    }
+
+}
