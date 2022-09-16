af5d98f335c858ebeb975da54920c09f1b7bcc19
Алексей <krysov-95@mail.ru>
2022-09-16 20:03:11 +0600

    added Registration2

--- src/test/java/guru/qa/tests/RegistrationFormTests.java
+++ src/test/java/guru/qa/tests/RegistrationFormTests.java
@@ -5,9 +5,8 @@ import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationFormTests {
     @BeforeAll
@@ -26,6 +25,25 @@ public class RegistrationFormTests {
         $("#userEmail").setValue("Alex@egorov.com");
  //       $("#gender-radio-3").click();
         $("#genterWrapper").$(new ByText("Other")).click();
+        $("#userNumber").setValue("1231231231");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2005");
+        $(".react-datepicker__day--028:not(.react-datepicker__day--outside-month)").click();
+     //   $$(".react-datepicker__day--028")
+     //           .filter(not(cssClass("react-datepicker__day--outside-month"))).first().click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(new ByText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue("Qa guru street 7");
+        $("#state").click();
+        $("#stateCity-wrapper").$(new ByText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(new ByText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Alex Egorov"),text("Alex@egorov.com"),text("28 July,2005"));
 
 
 
@@ -33,6 +51,7 @@ public class RegistrationFormTests {
 
 
 
+        $("#submit").click();
 
 
 
